3f56fe6d5193ea986e8290b995df09446549dca8
VIKAS AGARWAL <vkasbnsl888@gmail.com>
2019-08-04 20:42:16 +0530

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,10 +1,10 @@
 const config = {
   PORT: 6789,
   SQLdb: {
-    host: 'test.c0u1dvvflrig.ap-south-1.rds.amazonaws.com',
-    user: 'googledata',
-    password: 'K-#$XY=dS3Vf;?4V',
-    database: 'ambitionbox',
+    host: '',
+    user: '',
+    password: '',
+    database: '',
   },
 };
 module.exports = config;
